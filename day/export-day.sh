9aa3e20f70a7ac6245f8070470c5040f485a2bf2
JP Ungaretti <hello@jungaretti.com>
2022-03-20 22:54:58 -0700

    Add script for commit

--- day/export-day.sh
+++ day/export-day.sh
@@ -4,3 +4,11 @@ DATE=$(date +%F)
 
 pacman -Qqetn > $DATE-pkglist.txt
 pacman -Qqetm > $DATE-aurlist.txt
+
+if [ "$1" = '-c' ]; then
+    git add $DATE-pkglist.txt
+    git add $DATE-aurlist.txt
+    git commit -m "Backup $DATE"
+else
+    echo 'Add -c to commit all changes'
+fi
